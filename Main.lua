f1899e71012177a69ffcdd20324b0d1234af2e47
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-08 11:13:20 -0500

    Update and rename Main.lua (Unfinished lmao) to Main.lua

--- Main.lua
+++ Main.lua
@@ -4,7 +4,7 @@ local CampData = Data:GetDataStore("Money")
 local CampLevel = Data:GetStore("Level")
 game.Players.PlayerAdded:connect(function(plar)
 
-script.Parent.Level.Text="Level: "..Data:GetAsync("Level", script.Parent.IdEnter.Text)
+script.Parent.Level.Text="Level: "..CampLevel:GetAsync("Level", script.Parent.IdEnter.Text)
 script.Parent.CampStat.Text= CampData:GetAsync("Money, script.Parent.IdEnter.Text)
 
 local Banned = BanData:GetAsync("Banned", script.Parent.IdEnter.Text)
