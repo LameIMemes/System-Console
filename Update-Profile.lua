d991a7a6d8b165ae6bc7fc6411d015488a474fa1
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-09 07:34:11 -0500

    Update Update-Profile.lua

--- Update-Profile.lua
+++ Update-Profile.lua
@@ -3,3 +3,7 @@ local RE = game.ReplicatedStorage.ConsoleUpdate
 
 script.Parent.MouseButton1Click:connect(function()
   RE:FireServer()
+    script.Parent.Status.Text="Updating Profile"
+   wait(2)
+    script.Parent.Status.Text="Profile Updated."
+ 
