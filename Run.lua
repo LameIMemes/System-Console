71a9ce5123239f343edf5bc02e5ff860a03562a0
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-09 08:04:23 -0500

    Update Run.lua

--- Run.lua
+++ Run.lua
@@ -1,13 +1,12 @@
 -- Server Script scree -- 
 
-local LoserData = game:GetService("DataStoreService")
 local ui = plar:WaitForChild("PlayerGui")
 local Whitelist = "5069773"
 
 game.Players.PlayerAdded:connect(function(plar)
 plar:Chatted(function(msg)
 
-if plar.UserId== Whitelist and msg=="systemconsole" then
+if plar.UserId== Whitelist and msg=="console" then
 
 local Console = game.ServerStorage.MasterFolder.SystemConsole:Clone()
          Console.Parent= ui
