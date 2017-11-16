a83d632e78a2ecac52772f1a8cc1659bb0056877
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-16 08:02:48 -0500

    Update and rename Update-Profile.lua to Main/Update-Profile.lua

--- Main/Update-Profile.lua
+++ Main/Update-Profile.lua
@@ -4,6 +4,5 @@ local RE = game.ReplicatedStorage.ConsoleUpdate
 script.Parent.MouseButton1Click:connect(function()
   RE:FireServer()
     script.Parent.Status.Text="Updating Profile"
-   wait(2)
-    script.Parent.Status.Text="Profile Updated."
+    
  
