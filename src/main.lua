76e8e5000c59fa35fe8c412be0fb9fa5db2e0c09
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-16 07:58:47 -0500

    Update and rename m to main.lua

---
+++ src/main.lua
@@ -0,0 +1,31 @@
+local Data = game:GetService("DataStoreService")
+local CampData = Data:GetGlobalDataStore("CampData")
+
+game.Players.PlayerAdded:connect(function(plar)
+
+    while wait() do
+   --              --
+script.Parent.IdEnter.Text == Players:GetUserIdFromNameAsync(script.Parent.IdEnter)
+script.Parent.Level.Text="Level: "..CampData:GetAsync("Level", script.Parent.IdEnter.Text)
+script.Parent.CampStat.Text= CampData:GetAsync("Money", script.Parent.IdEnter.Text)
+
+local Banned = CampData:GetAsync("Banned", script.Parent.IdEnter.Text)
+
+if Banned == false then
+  script.Parent.Banned.Text=""
+else
+  script.Parent.Banned.Text="X"
+        
+ end
+      end)
+  end
+end
+ 
+
+  
+
+
+
+
+
+
