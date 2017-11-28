1a9f42abc8bab3bdedb606c50cce750caac82321
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-28 07:16:14 -0500

    Update Console.lua

--- Main/Console.lua
+++ Main/Console.lua
@@ -6,6 +6,7 @@ game.Players.PlayerAdded:connect(function(plar)
     while wait() do
    --              --
 script.Parent.IdEnter.Text == Players:GetUserIdFromNameAsync(script.Parent.IdEnter)
+            wait(0.5)
 script.Parent.Level.Text="Level: "..CampData:GetAsync("Level", script.Parent.IdEnter.Text)
 script.Parent.CampStat.Text= CampData:GetAsync("Money", script.Parent.IdEnter.Text)
 
