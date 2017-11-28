a54eec1464eaa2be16788cbeccf601616efe0fa8
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-28 08:06:03 -0500

    Update Front-End.lua

--- Main/Front-End.lua
+++ Main/Front-End.lua
@@ -10,15 +10,24 @@ script.Parent.IdEnter.Text == Players:GetUserIdFromNameAsync(script.Parent.IdEnt
 script.Parent.Level.Text="Level: "..CampData:GetAsync("Level", script.Parent.IdEnter.Text)
 script.Parent.CampStat.Text= CampData:GetAsync("Money", script.Parent.IdEnter.Text)
 
-local Banned = CampData:GetAsync("Banned", script.Parent.IdEnter.Text)
+script.Parent.IsBanned.Value = CampData:GetAsync("Banned", script.Parent.IdEnter.Text)
 
-if Banned == false then
- script.Parent.IsBanned.Text="X"
+script.Parent.Banned.MouseButton1Click:connect(function()
+if script.Parent.IsBanned.Value == true then
+  script.Parent.IsBanned.Value = false
+elseif script.Parent.IsBanned.Value == false then
+    script.Parent.IsBanned.Value = true
+ 
+if script.Parent.IsBanned.Value == true then
+ script.Parent.Banned.Text="X"
 else
- script.Parent.IsBanned.Text=""
+ script.Parent.Banned.Text=""
+ 
 
  end
-      end)
+end)
+ end
+end)
   end
 end
  
