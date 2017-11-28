244ea5a322ba203afc0c05b3bb00b3dc138f69b5
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-11-28 07:41:58 -0500

    Update Front-End.lua

--- Main/Front-End.lua
+++ Main/Front-End.lua
@@ -6,11 +6,16 @@ game.Players.PlayerAdded:connect(function(plar)
     while wait() do
    --              --
 script.Parent.IdEnter.Text == Players:GetUserIdFromNameAsync(script.Parent.IdEnter)
-            wait(0.5)
+            wait()
 script.Parent.Level.Text="Level: "..CampData:GetAsync("Level", script.Parent.IdEnter.Text)
 script.Parent.CampStat.Text= CampData:GetAsync("Money", script.Parent.IdEnter.Text)
 
 local Banned = CampData:GetAsync("Banned", script.Parent.IdEnter.Text)
+
+if Banned == false then
+ script.Parent.IsBanned.Text="X"
+else
+ script.Parent.IsBanned.Text=""
 
  end
       end)
