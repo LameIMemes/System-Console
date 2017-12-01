24d8383a72343f8ab32ed727bef096840a92f9c8
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-12-01 11:45:12 -0500

    Update Server-Update.lua

--- Main/Server-Update.lua
+++ Main/Server-Update.lua
@@ -1,6 +1,5 @@
 local Data = game:GetService("DataStoreService")
 local CampData = Data:GetDataStore("CampData")
-local CampLevel = CampData:GetDataStore("Level")
 local BanData = CampData:GetDataStore("Banned")
 local Release = CampData:GetDataStore("Release_Date")
 local CampDolla = CampData:GetDataStore("Money")
@@ -11,7 +10,7 @@ local CampDolla = CampData:GetDataStore("Money")
 game.ReplicatedStorage.ConsoleUpdate.OnServerEvent:connect(function()
 Local key = "user_"..script.Parent.IdEnter.Text
     CampDolla:UpdateAsync(key, script.Parent.MoneyEdit.Text)
-    CampLevel:UpdateAsync(key, script.Parent.LevelEdit.Text)
+    CampData:UpdateAsync(key, script.Parent.LevelEdit.Text)
     BanData:UpdateAsync(key, script.Parent.IsBanned.Value)
     Release:UpdateAsync(key, script.Parent.ReleaseDate.Text)
           
