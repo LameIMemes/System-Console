630e1f9f286fff762a2f3fabf103003b8dabfb67
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-12-04 13:16:07 -0500

    Update Server-Update.lua

--- Main/Server-Update.lua
+++ Main/Server-Update.lua
@@ -3,6 +3,7 @@ local CampData = Data:GetDataStore("CampData")
 local BanData = CampData:GetDataStore("Banned")
 local Release = CampData:GetDataStore("Release_Date")
 local CampDolla = CampData:GetDataStore("Money")
+local Comment = CampData:GetDataStore("Reason")
 
 
 --    --
@@ -13,6 +14,7 @@ Local key = "user_"..script.Parent.IdEnter.Text
     CampData:UpdateAsync(key, script.Parent.LevelEdit.Text)
     BanData:UpdateAsync(key, script.Parent.IsBanned.Value)
     Release:UpdateAsync(key, script.Parent.ReleaseDate.Text)
+    Comment:UpdateAsync(key, script.Parent.ReasonBanned.Text)
  script.Parent.Status.Text="Profile Updated!"
  end
    end
