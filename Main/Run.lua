3c28c061c45799c296a0a7e95ee563cc8f5c0928
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-12-04 07:37:11 -0500

    Update Run.lua

--- Main/Run.lua
+++ Main/Run.lua
@@ -6,7 +6,7 @@ local Whitelist = "5069773"
 game.Players.PlayerAdded:connect(function(plar)
 plar.Chatted:connect(function(msg)
 
-if plar.UserId== Whitelist and msg=="console" then
+if plar.UserId== Whitelist and msg=="sysconsole" then
 
 local Console = game.ServerStorage.MasterFolder.SystemConsole:Clone()
          Console.Parent= ui
