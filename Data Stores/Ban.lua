cd08c54e397d34cba0316ac3ff099e91ec3f44f6
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-12-05 10:45:57 -0500

    Update Ban.lua

--- Data Stores/Ban.lua
+++ Data Stores/Ban.lua
@@ -2,10 +2,10 @@ local Ban = game:GetService("DataStoreService"):GetDataStore("Banned")
 
 game.Players.PlayerAdded:connect(function(plar)
 
-local Ban = {}
- Ban = false
+local BanData = {}
+ Banned = false
+  
+-- To be continued xd'dx    --
  
- 
-if Ban == false then 
-  else
+
  
