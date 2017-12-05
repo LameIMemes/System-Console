afa2793adb7571d186d3d43f1072e0154d54e953
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2017-12-05 11:45:23 -0500

    Update Server-Update.lua

--- Main/Server-Update.lua
+++ Main/Server-Update.lua
@@ -10,15 +10,34 @@ local Comment = CampData:GetDataStore("Reason")
 
 game.ReplicatedStorage.ConsoleUpdate.OnServerEvent:connect(function()
 Local key = "user_"..script.Parent.IdEnter.Text
-    CampDolla:UpdateAsync(key, script.Parent.MoneyEdit.Text)
-    CampData:UpdateAsync(key, script.Parent.LevelEdit.Text)
-    BanData:UpdateAsync(key, script.Parent.IsBanned.Value)
-    Release:UpdateAsync(key, script.Parent.ReleaseDate.Text)
-    Comment:UpdateAsync(key, script.Parent.ReasonBanned.Text)
- script.Parent.Status.Text="Profile Updated!"
+    CampDolla:UpdateAsync(key, function(MoneyValue)
+    CampData:UpdateAsync(key, funtion(LevelValue)
+    BanData:UpdateAsync(key, function(Banned)
+    Release:UpdateAsync(key, function(ReleaseDate)
+    Comment:UpdateAsync(key, funtion(Comments)
+ 
+ local newMoney = MoneyValue or 0
+  newMoney = newMoney + script.Parent.MoneyEdit.Text
+local newLevel = LevelValue or 0
+  newLevel = newMoney + script.Parent.LevelEdit.Text
+local ChangedBan = Banned or nil
+  ChangedBan = script.Parent.IsBanned.Value
+local Date_Release = ReleaseDate or nil
+   Date_Release = script.Parent.ReleaseDate.Text
+local Reasons = Comment or ""
+   Reasons = script.Parent.ReasonBanned.Text
+                                        
+return newMoney, newLevel, ChangedBan, Date_Release, Reasons
+    wait()
+script.Parent.Status.Text="Profile Updated!"
  end
    end
 end
+end)
+                    end)
+            end)
+    end)
+end)
 end)
 
     
