c8b1352def3b8eccf4763391beb866ab57ad9500
Lame_Memes <31599790+LameIMemes@users.noreply.github.com>
2018-03-01 10:52:29 -0500

    Update Front-End.lua

--- Main/Front-End.lua
+++ Main/Front-End.lua
@@ -1,39 +1,20 @@
 local Data = game:GetService("DataStoreService")
-local Level = Data:GetDataStore("CampData")
+local Reason = Data:GetDataStore("Reason")
 local Money = Data:GetDataStore("Money")
-local Banned = Data:GetDataStore("Banned")
-local Release = Data:GetDataStore("Release_Date")
-local DateBanned = Data:GetDataStore("Date_Banned")
-local Comment = Data:GetDataStore("Reason")
+local Time = Data:GetDataStore("TimeLeft")
+local lev = Data:GetDataStore("Level")
+local Rank = Data:GetDataStore("Ronk")
+local C1 = Data:GetDataStore("Color1")
+local C1 = Data:GetDataStore("Color2")
+local C1 = Data:GetDataStore("Color3")
 
 game.Players.PlayerAdded:connect(function(plar)
    --              --
+      script.Parent.Enter.MouseButton1Click:connect(function()
 script.Parent.IdEnter.Text == Players:GetUserIdFromNameAsync(script.Parent.IdEnter.Text)
-            wait()
-script.Parent.LevelEdit.Text= CampData:GetAsync(script.Parent.IdEnter.Text)
-    script.Parent.MoneyEdit.Text= Money:GetAsync(script.Parent.IdEnter.Text)
-        script.Parent.IsBanned.Value = Banned:GetAsync(script.Parent.IdEnter.Text)
-            script.Parent.ReleaseDate.Text = Release:GetAsync(script.Parent.IdEnter.Text)
-        script.Parent.BanDate.Text = DateBanned:GetAsync(script.Parent.IdEnter.Text)
-      script.Parent.ReasonBanned.Text = Comment:GetAsync(script.Parent.IdEnter.Text)
-
-script.Parent.Banned.MouseButton1Click:connect(function()
-if script.Parent.IsBanned.Value == true then
-  script.Parent.IsBanned.Value = false
-elseif script.Parent.IsBanned.Value == false then
-    script.Parent.IsBanned.Value = true
- 
-if script.Parent.IsBanned.Value == true then
- script.Parent.Banned.Text="X"
-else
- script.Parent.Banned.Text=""
- 
-
- end
-end)
- end
-end)
-  end
+    
+--                 --
+    
 end
  
 
